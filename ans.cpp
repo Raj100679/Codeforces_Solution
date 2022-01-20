f4dbe43ff875086be7f9f302e94a36235a0aa99e
Raj100679 <85561967+Raj100679@users.noreply.github.com>
2022-01-20 12:52:53 +0530

    1337 B. Kana and Dragon Quest game.cpp

---
+++ ans.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int int64_t
+
+void solve()
+{
+  int x, n, m;
+  cin >> x >> n >> m;
+  while (n != 0)
+  {
+   if(x<=10 && m>0){
+     x=x-10;
+     m--;
+     break;
+   }
+   else{
+     x=(x/2)+10;
+     n--;
+   }
+  }
+  while(m!=0){
+    x=x-10;
+    m--;
+  }
+
+  if (x > 0)
+  {
+    cout << "NO" << endl;
+  }
+  else
+  {
+    cout << "YES" << endl;
+  }
+}
+int32_t main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+
+  return 0;
+}
